0ba38178ecbad787f3fa4868acab8360b1bd9724
Nelite <neliitsme14@gmail.com>
2022-11-22 20:06:24 +0300

    refactor: rename entry file and class

--- src/main/kotlin/com/itmo/se/recommendationservice/RecommendationServiceApplication.kt
+++ src/main/kotlin/com/itmo/se/recommendationservice/RecommendationServiceApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MvcRestMavenKotlinJpaApplication
+class RecommendationServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<MvcRestMavenKotlinJpaApplication>(*args)
+    runApplication<RecommendationServiceApplication>(*args)
 }
--- src/test/kotlin/com/itmo/se/recommendationservice/RecommendationServiceApplicationTests.kt
+++ src/test/kotlin/com/itmo/se/recommendationservice/RecommendationServiceApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MvcRestMavenKotlinJpaApplicationTests {
+class RecommendationServiceApplicationTests {
 
     @Test
     fun contextLoads() {
