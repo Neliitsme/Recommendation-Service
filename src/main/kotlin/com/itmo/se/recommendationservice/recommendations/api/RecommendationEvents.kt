dc438bcdf85349881c2e8db691ec4fe88bb51be8
Nelite <neliitsme14@gmail.com>
2022-11-25 21:31:50 +0300

    feat: add Recommendation service events

--- src/main/kotlin/com/itmo/se/recommendationservice/recommendations/api/RecommendationEvents.kt
+++ src/main/kotlin/com/itmo/se/recommendationservice/recommendations/api/RecommendationEvents.kt
@@ -1,4 +1,32 @@
 package com.itmo.se.recommendationservice.recommendations.api
 
-class RecommendationEvents {
-}
+import ru.quipy.core.annotations.DomainEvent
+import ru.quipy.domain.Event
+import java.util.*
+
+const val RECOMMENDATION_CREATED = "RECOMMENDATION_CREATED_EVENT"
+
+const val USER_SEEN_ITEM_CREATED = "USER_SEEN_ITEM_CREATED_EVENT"
+const val USER_SEEN_ITEM_COEFFICIENT_CHANGED = "USER_SEEN_ITEM_COEFFICIENT_CHANGED_EVENT"
+
+
+@DomainEvent(name = RECOMMENDATION_CREATED)
+data class RecommendationCreatedEvent(
+    val recommendationId: UUID,
+    val ownerId: UUID,
+) : Event<RecommendationAggregate>(name = RECOMMENDATION_CREATED)
+
+@DomainEvent(name = USER_SEEN_ITEM_CREATED)
+data class UserSeenItemCreatedEvent(
+    val seenItemId: UUID,
+    val itemId: UUID,
+    val coefficient: Int
+) : Event<RecommendationAggregate>(name = USER_SEEN_ITEM_CREATED)
+
+@DomainEvent(name = USER_SEEN_ITEM_COEFFICIENT_CHANGED)
+data class UserSeenItemCoefficientChangedEvent(
+    val seenItemId: UUID,
+    val itemId: UUID,
+    val newCoefficient: Int
+) :
+    Event<RecommendationAggregate>(name = USER_SEEN_ITEM_COEFFICIENT_CHANGED)
