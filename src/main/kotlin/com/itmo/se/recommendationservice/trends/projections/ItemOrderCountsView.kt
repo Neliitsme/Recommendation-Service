08b812ad1e84f7d55588c5300968a1c35c51e26e
Alexey <71148093+lehatheslayer@users.noreply.github.com>
2022-12-03 14:51:50 +0300

    ItemOrderCountsView (#12)
    * realized ItemOrderCountsView: service subscribes to AddItemToOrderEvent and RemoveItemFromOrderEvent
    
    * feat: return repository annotation
    
    Co-authored-by: Nelite <neliitsme14@gmail.com>

--- src/main/kotlin/com/itmo/se/recommendationservice/trends/projections/ItemOrderCountsView.kt
+++ src/main/kotlin/com/itmo/se/recommendationservice/trends/projections/ItemOrderCountsView.kt
@@ -1,17 +1,20 @@
 package com.itmo.se.recommendationservice.trends.projections
 
+import com.itmo.se.recommendationservice.orders.api.OrderAggregate
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
+import ru.quipy.core.annotations.DomainEvent
+import ru.quipy.domain.Event
 import ru.quipy.streams.AggregateSubscriptionsManager
 import java.util.*
 import javax.annotation.PostConstruct
-import javax.persistence.Column
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
+
+const val ADD_ITEM_TO_ORDER_EVENT = "ADD_ITEM_TO_ORDER"
+const val REMOVE_ITEM_FROM_ORDER_EVENT = "REMOVE_ITEM_FROM_ORDER"
 
 @Component
 class ItemOrderCountsView(
@@ -22,6 +25,38 @@ class ItemOrderCountsView(
 
     @PostConstruct
     fun init() {
+        subscriptionsManager.createSubscriber(OrderAggregate::class, "item-order-counts-increase") {
+            `when`(AddItemToOrderEvent::class) { event ->
+                val fromDb: Optional<ItemOrderCounts> = itemOrderCountsRepository.findByItemId(event.itemId)
+
+                fromDb.ifPresentOrElse(
+                    { entity ->
+                        entity.orderedTimes += event.countItem
+                        itemOrderCountsRepository.save(entity)
+                    },
+                    {
+                        val toSave = ItemOrderCounts(itemId = event.itemId, orderedTimes = event.countItem)
+                        itemOrderCountsRepository.save(toSave)
+                    }
+                )
+                logger.info("The item with id=${event.itemId}, count=${event.countItem} was added to the order")
+            }
+        }
+
+        subscriptionsManager.createSubscriber(OrderAggregate::class, "item-order-counts-decrease") {
+            `when`(RemoveItemFromOrderEvent::class) { event ->
+                val fromDb = itemOrderCountsRepository.findByItemId(event.itemId)
+                // Optional всегда isPresent, т.к. чтобы убрать товар(ы) из
+                // корзины нужно для начале его(их) туда добавить
+                // Думаю, что ситуаций, когда toSave.orderedTimes - event.countItem < 0
+                // по упомянутой выше причине
+                val toSave = fromDb.get()
+                toSave.orderedTimes -= event.countItem
+
+                itemOrderCountsRepository.save(toSave)
+                logger.info("The item with id=${event.itemId}, count=${event.countItem} was removed from the order")
+            }
+        }
     }
 }
 
@@ -35,8 +70,26 @@ data class ItemOrderCounts(
     var itemId: UUID? = null,
 
     @Column
-    var orderedTimes: Int = 0
+    var orderedTimes: Long = 0
 )
 
 @Repository
-interface ItemOrderCountsRepository : JpaRepository<ItemOrderCounts, UUID>
+interface ItemOrderCountsRepository : JpaRepository<ItemOrderCounts, UUID> {
+    fun findByItemId(itemId: UUID): Optional<ItemOrderCounts>
+}
+
+
+@DomainEvent(name = ADD_ITEM_TO_ORDER_EVENT)
+data class AddItemToOrderEvent(
+    val itemId: UUID,
+    val countItem: Long,
+    val orderId: UUID,
+    val userId: UUID
+) : Event<OrderAggregate>(name = ADD_ITEM_TO_ORDER_EVENT)
+
+@DomainEvent(name = REMOVE_ITEM_FROM_ORDER_EVENT)
+data class RemoveItemFromOrderEvent(
+    val itemId: UUID,
+    val countItem: Long,
+    val orderId: UUID
+) : Event<OrderAggregate>(name = REMOVE_ITEM_FROM_ORDER_EVENT)
