21308ca6cc29c3c78fabe5c2d2519a2a40d7fd85
fedos3d <fedos3d@gmail.com>
2022-12-09 20:54:22 +0000

    created test class

---
+++ src/test/kotlin/com/itmo/se/recommendationservice/recommendations/controller/RecommendationControllerTest.kt
@@ -0,0 +1,35 @@
+package com.itmo.se.recommendationservice.recommendations.controller
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RecommendationControllerTest {
+
+
+    @Test
+    fun createPersonalRecommendation() {
+
+    }
+
+    @Test
+    fun createSeenItem() {
+
+    }
+
+    @Test
+    fun changeItemRecommendationCoefficient() {
+    }
+
+    @Test
+    fun getTrendingItems() {
+    }
+
+    @Test
+    fun getRecommendationForItem() {
+    }
+
+    @Test
+    fun getRecommendationEsService() {
+    }
+}
