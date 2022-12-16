432475ff936742211f04dbc9caea22c318e1bdd9
Nelite <neliitsme14@gmail.com>
2022-12-16 21:59:10 +0300

    chore: remove unused file

--- src/main/kotlin/com/itmo/se/recommendationservice/orders/api/OrderAggregate.kt
+++
@@ -1,8 +0,0 @@
-package com.itmo.se.recommendationservice.orders.api
-
-import ru.quipy.core.annotations.AggregateType
-import ru.quipy.domain.Aggregate
-
-// TODO ???
-@AggregateType(aggregateEventsTableName = "orders")
-class OrderAggregate : Aggregate
